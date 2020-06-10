8a3a0e0ad2ef4bcc02c630e7b89b89dcc14b8ca2
Anthony George <ageorge530@gmail.com>
2020-06-10 06:23:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,25 @@
 class Artist
-  attr_accessor :name
+  @@all = []
+  attr_accessor :name, :song
  
   def initialize(name)
     @name = name
   end
 
+def  self.all
+  @@all
+end
 
+def add_song(song)
+  song.artist = self
+end
  
+ def song
+   song.all.select {|song| song.artist == self}
+ end
+ 
+ def self.find_or_create_by_name(name)
+   self.find(name) ? self.find(name) :self.new(name)
+end 
+   
 end
