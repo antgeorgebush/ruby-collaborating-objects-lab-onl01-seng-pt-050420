3c2992cfb3a480b341384977f08cfa3ab6655467
Anthony George <ageorge530@gmail.com>
2020-06-10 06:24:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,5 +21,11 @@ end
  def self.find_or_create_by_name(name)
    self.find(name) ? self.find(name) :self.new(name)
 end 
+
+def self.find(name)
+  self.all.find {|artist| artist.name == name }
+end
+
+
    
 end
