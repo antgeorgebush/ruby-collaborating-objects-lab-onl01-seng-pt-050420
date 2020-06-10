d59fb16358ecefdd24dbb6baa208db4f771d2511
Anthony George <ageorge530@gmail.com>
2020-06-10 06:36:58 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,9 +1,17 @@
-class MP3Importer 
-  attr_reader :path  
-  
+
+class MP3Importer
+  attr_reader :path
+
   def initialize(path)
     @path = path
   end
-  
+
   def files
-    @files ||= Dir
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+  end
+
+  def import
+    files.each{|f| Song.new_by_filename(f)}
+  end
+end
+
